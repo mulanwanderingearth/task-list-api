0b1d5e6584402fad425c8e835c65c945de6aa7c9
Xin Li <happyxin331@icloud.com>
2025-11-02 00:31:32 -0700

    wave 1 finished

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,24 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from typing import Optional
+from datetime import datetime
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[Optional[datetime]] = mapped_column(nullable=True)
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": self.completed_at is not None
+        }
+    @classmethod
+    def from_dict(cls,data):
+        return cls(title=data["title"],
+                    description=data["description"],
+                    completed_at=data.get("completed_at"))
+            
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,77 @@
-from flask import Blueprint
+from flask import Blueprint,abort, make_response, request,Response
+from ..db import db
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+from app.models.task import Task
+
+@tasks_bp.post("")
+def create_task():
+    request_body = request.get_json()
+    
+    try:
+        title = request_body["title"]
+        description = request_body["description"]
+    except KeyError:
+        return {"details": "Invalid data"}, 400
+    
+    completed_at = request_body.get("completed_at")
+    
+    new_task = Task.from_dict(
+        request_body
+    )
+    db.session.add(new_task)
+    db.session.commit()
+
+    return new_task.to_dict(), 201
+
+@tasks_bp.get("")
+def get_all_tasks():
+    query = db.select(Task)
+    tasks = db.session.scalars(query)
+
+    if not tasks:
+         return {"details": "No tasks found"}, 404
+    
+    return [task.to_dict() for task in tasks]
+
+
+@tasks_bp.get("/<task_id>")
+def get_one_task(task_id):
+    task = validate_task(task_id)
+    return task.to_dict()
+
+@tasks_bp.put("/<task_id>")
+def update_one_task(task_id):
+    task = validate_task(task_id)
+    request_body = request.get_json()
+    task.title = request_body.get("title")
+    task.description = request_body.get("description")
+    task.completed_at = request_body.get("completed_at")
+
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        response = {"details": f"task {task_id} invalid"}
+        abort(make_response(response, 400))
+
+    query = db.select(Task).where(Task.id == task_id)
+    task = db.session.scalar(query)
+
+    if not task:
+        response = {"details": f"Task {task_id} not found"}
+        abort(make_response(response, 404))
+
+    return task
+
+@tasks_bp.delete("/<task_id>")
+def delete_one_task(task_id):
+    task = validate_task(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
